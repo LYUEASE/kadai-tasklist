2b5beaaa3511bf0727af432fca5640a6b3226c4e
ryuhei.tomita <LYUEASE1@gmail.com>
2018-09-15 13:24:21 +0000

    login

--- app/controllers/tasks_controller.rb
+++ app/controllers/tasks_controller.rb
@@ -1,5 +1,6 @@
 class TasksController < ApplicationController
-  
+    before_action :require_user_logged_in
+    
   def index
       @tasks = Task.all
   end
@@ -15,16 +16,16 @@ class TasksController < ApplicationController
   end
   
   
-  
   def create
-    @task = Task.new(task_params)
+    @task = current_user.tasks.build(task_params)
 
    if @task.save
      flash[:success] = 'タスクが正常に登録されました'
-     redirect_to @task 
+     redirect_to root_url
    else
+     @tasks = current_user.tasks.order('created_at DESC').page(params[:page])
      flash[:danger] = 'タスクが登録されませんでした'
-     render :new
+     render 'toppages/index'
    end
   end
   
@@ -52,7 +53,7 @@ class TasksController < ApplicationController
     @task.destroy
     
     flash[:success] = 'タスクは正常に削除されました'
-    redirect_to tasks_url
+    redirect_to root_url
   end
   
   
